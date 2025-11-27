bbe53064333a625f52e024cd5faedfab13a21cd5
kumaroid <andr-ushka2@yandex.ru>
2025-11-28 02:10:13 +0300

    fix: fix settings

--- vihorki/infrastructure/settings.py
+++ vihorki/infrastructure/settings.py
@@ -1,10 +1,10 @@
 import os
 
 
-DB_USER = str(os.getenv('POSTGRES_USER'))
-DB_PASSWORD = str(os.getenv('POSTGRES_PASSWORD'))
-DB_HOST = str(os.getenv('POSTGRES_HOST'))
-DB_NAME = str(os.getenv('POSTGRES_DB'))
+DB_USER = os.getenv('POSTGRES_USER')
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+DB_HOST = os.getenv('POSTGRES_HOST')
+DB_NAME = os.getenv('POSTGRES_DB')
 DB_PORT = os.getenv('POSTGRES_PORT')
 
 DB_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
