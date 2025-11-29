b06f19ec76594868b450a82c950854c7971f3f91
kumaroid <andr-ushka2@yandex.ru>
2025-11-29 03:56:18 +0300

    feat: add redis to compose

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import logging
 from aiohttp import web
 
 from vihorki.infrastructure.postgres.on_startup.run_db import init_db_and_tables
+from vihorki.infrastructure.redis.redis_tools import redis_conn_context, RedisCache
+
+from vihorki.infrastructure.settings import REDIS_HOST, REDIS_IS_CLUSTER, REDIS_PASSWORD, REDIS_PORT, REDIS_USER
 
 logger = logging.getLogger(__name__)
 
@@ -19,6 +22,16 @@ async def on_startup(app):
 
 async def healthcheck(request):
     health_data = {'status': 'healthy', 'timestamp': asyncio.get_event_loop().time(), 'service': 'vihorki'}
+    async with redis_conn_context(
+        redis_host=REDIS_HOST,
+        redis_port=REDIS_PORT,
+        redis_user=REDIS_USER,
+        redis_password=REDIS_PASSWORD,
+        redis_is_cluster=REDIS_IS_CLUSTER,
+    ) as redis_cache:
+        cache = RedisCache(redis_cache)
+        await cache.set_value('healthcheck', health_data['timestamp'])
+
     return web.json_response(health_data)
 
 
--- vihorki/infrastructure/settings.py
+++ vihorki/infrastructure/settings.py
@@ -1,17 +1,15 @@
 import os
 
 
-# Имя хоста — это имя сервиса в docker-compose
-DB_HOST = os.getenv('DB_HOST', 'postgres')  # ← не "localhost"!
-DB_PORT = os.getenv('DB_PORT', '5432')  # ← внутри сети Docker — стандартный порт 5432
-
+DB_HOST = os.getenv('DB_HOST', 'postgres')
+DB_PORT = os.getenv('DB_PORT', '5432')
 DB_USER = os.getenv('POSTGRES_USER', 'habrpguser')
 DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'pgpwd4habr')
 DB_NAME = os.getenv('POSTGRES_DB', 'habrdb')
 DB_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-REDIS_HOST = os.getenv('REDIS_HOST')
-REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
+REDIS_PORT = os.getenv('REDIS_PORT', '6379')
 REDIS_USER = os.getenv('REDIS_USER')
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
 REDIS_IS_CLUSTER = os.getenv('REDIS_IS_CLUSTER')
